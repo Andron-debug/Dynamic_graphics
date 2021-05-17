b1e221b298c35345ffd96363dcc3652bc52b36c8
Andron-debug <b.a.a.2002@yandex.ru>
2021-05-17 09:49:54 +0300

    Созданы элементы формы

--- Dynamic_graphics/Form1.Designer.cs
+++ Dynamic_graphics/Form1.Designer.cs
@@ -29,20 +29,59 @@ namespace Dynamic_graphics
         /// </summary>
         private void InitializeComponent()
         {
+            this.Start_button = new System.Windows.Forms.Button();
+            this.Pause_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Start_button
+            // 
+            this.Start_button.Location = new System.Drawing.Point(630, 43);
+            this.Start_button.Name = "Start_button";
+            this.Start_button.Size = new System.Drawing.Size(94, 29);
+            this.Start_button.TabIndex = 0;
+            this.Start_button.Text = "Пуск";
+            this.Start_button.UseVisualStyleBackColor = true;
+            // 
+            // Pause_button
+            // 
+            this.Pause_button.Location = new System.Drawing.Point(630, 106);
+            this.Pause_button.Name = "Pause_button";
+            this.Pause_button.Size = new System.Drawing.Size(94, 29);
+            this.Pause_button.TabIndex = 1;
+            this.Pause_button.Text = "Пауза";
+            this.Pause_button.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 381);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Pause_button);
+            this.Controls.Add(this.Start_button);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Start_button;
+        private System.Windows.Forms.Button Pause_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
